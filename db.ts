27903c216c0a8a68388e324ca1f8312ea4388e28
Ruzaik Rafeek <ruzaik.rafeek@gmail.com>
2025-01-19 20:06:37 +0530

    Fix environment variable setup

--- db.ts
+++ db.ts
@@ -2,6 +2,6 @@ import { drizzle } from 'drizzle-orm/neon-serverless';
 import ws from 'ws';
 
 export const db = drizzle({
-  connection: "postgresql://neondb_owner:EslOijvI70kJ@ep-misty-cherry-a1ybs4ib.ap-southeast-1.aws.neon.tech/neondb?sslmode=require",
+  connection: process.env.DATABASE_URL!,
   ws: ws,
 });
